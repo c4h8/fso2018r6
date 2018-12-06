209c38396263c8661a46fc002fc291bfc0f85779
Panu Liesiö <panu.liesio@aalto.fi>
2018-12-06 21:09:19 +0200

    complete 6.19-6.23

--- routed-anecdotes/src/App.js
+++ routed-anecdotes/src/App.js
@@ -1,51 +1,70 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Link, NavLink } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 
 const AnecdoteList = ({ anecdotes }) => (
-  <div>
-    <h2>Anecdotes</h2>
-    <ul>
-      {anecdotes.map(anecdote =>
-        <li key={anecdote.id}>
-          <Link to={`/anecdotes/${anecdote.id}`}> {anecdote.content} </Link>
-        </li>
-      )}
-    </ul>  
+  <div className="row">
+    <div className="col-sm-12 py-3">
+      <h2>Anecdotes</h2>
+      <ul className="list-group">
+        {anecdotes.map(anecdote =>
+          <li className="list-group-item" key={anecdote.id}>
+            <Link to={`/anecdotes/${anecdote.id}`}> {anecdote.content} </Link>
+          </li>
+        )}
+      </ul>  
+    </div>
   </div>
 )
 
 const Anecdote = ({ anecdote: { author, content, id, info, votes }}) => (
-  <div>
-    <h2>{content} by {author}</h2>
-    <p>has {votes} votes</p>
-    <p>for more info see <a href={info}>{info}</a></p>
+  <div className="row">
+    <div className="col-sm-12 py-3">
+      <h2>{content} by {author}</h2>
+      <p>has <span class="badge badge-pill badge-success mx-1">{votes}</span> votes
+      </p>
+      <p>for more info see <a href={info}>{info}</a></p>
+    </div>
   </div>
 )
 
 const Notification = ({message}) => (
-  message ? <div>{message}</div> : null
+  message ? <div className="alert alert-success" role="alert">{message}</div> : null
 )
 
 const About = () => (
-  <div>
-    <h2>About anecdote app</h2>
-    <p>According to Wikipedia:</p>
-    
-    <em>An anecdote is a brief, revealing account of an individual person or an incident. 
-      Occasionally humorous, anecdotes differ from jokes because their primary purpose is not simply to provoke laughter but to reveal a truth more general than the brief tale itself, 
-      such as to characterize a person by delineating a specific quirk or trait, to communicate an abstract idea about a person, place, or thing through the concrete details of a short narrative. 
-      An anecdote is "a story with a point."</em>
+  <React.Fragment>
+    <div className="row">
+      <div className="col-sm-8 py-3">
+        <h2>About anecdote app</h2>
+      </div>
+    </div>
+    <div className="row">
+      <div className="col-sm-8">
+        <p>According to Wikipedia:</p>
+        
+        <em>An anecdote is a brief, revealing account of an individual person or an incident. 
+          Occasionally humorous, anecdotes differ from jokes because their primary purpose is not simply to provoke laughter but to reveal a truth more general than the brief tale itself, 
+          such as to characterize a person by delineating a specific quirk or trait, to communicate an abstract idea about a person, place, or thing through the concrete details of a short narrative. 
+          An anecdote is "a story with a point."</em>
 
-    <p>Software engineering is full of excellent anecdotes, at this app you can find the best and add more.</p>
-  </div>
+        <p>Software engineering is full of excellent anecdotes, at this app you can find the best and add more.</p>
+      </div>
+      <div className="col-sm-4">
+        <img className="img-fluid rounded" src="https://upload.wikimedia.org/wikipedia/commons/a/a1/Alan_Turing_Aged_16.jpg" />
+      </div>
+    </div>
+  </React.Fragment>
 )
 
 const Footer = () => (
-  <div>
+  <div className="row">
+    <div className="col-sm-12 py-3">
     Anecdote app for <a href='https://courses.helsinki.fi/fi/TKT21009/121540749'>Full Stack -sovelluskehitys</a>.
 
     See <a href='https://github.com/mluukkai/routed-anecdotes'>https://github.com/mluukkai/routed-anecdotes</a> for the source code. 
+  </div>
   </div>
 )
 
@@ -78,23 +97,25 @@ class CreateNew extends React.Component {
 
   render() {
     return(
-      <div>
+      <div className="row">
+        <div className="col-sm-12 py-3">
         <h2>create a new anecdote</h2>
         <form onSubmit={this.handleSubmit}>
-          <div>
+          <div className="form-group">
             content 
-            <input name='content' value={this.state.content} onChange={this.handleChange} />
+            <input className="form-control" name='content' value={this.state.content} onChange={this.handleChange} />
           </div>
-          <div>
+          <div className="form-group">
             author
-            <input name='author' value={this.state.author} onChange={this.handleChange} />
+            <input className="form-control" name='author' value={this.state.author} onChange={this.handleChange} />
           </div>
-          <div>
+          <div className="form-group">
             url for more info
-            <input name='info' value={this.state.info} onChange={this.handleChange} />
+            <input className="form-control" name='info' value={this.state.info} onChange={this.handleChange} />
           </div> 
-          <button>create</button>
+          <button className="btn btn-primary">create</button>
         </form>
+        </div>
       </div>  
     )
 
@@ -111,7 +132,7 @@ class App extends React.Component {
           content: 'If it hurts, do it more often',
           author: 'Jez Humble',
           info: 'https://martinfowler.com/bliki/FrequencyReducesDifficulty.html',
-          votes: 0,
+          votes: 23,
           id: '1'
         },
         {
@@ -153,15 +174,17 @@ class App extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className="container">
         <h1>Software anecdotes</h1>
         <Router>
           <div>
-            <div>    
-              <Link to="/">anecdotes</Link> &nbsp;
-              <Link to="/create">create new</Link> &nbsp;
-              <Link to="/about">about</Link> &nbsp;
-            </div>
+            <nav className="navbar navbar-expand-sm navbar-dark bg-dark"> 
+              <div className="navbar-nav mt-2 mt-sm-0">
+              <NavLink className="nav-link" exact to="/">anecdotes</NavLink> &nbsp;
+              <NavLink className="nav-link" to="/create">create new</NavLink> &nbsp;
+              <NavLink className="nav-link" to="/about">about</NavLink> &nbsp;
+              </div>
+            </nav>
             <Notification message={this.state.notification} />
 
             <Route exact path="/" render={() => <AnecdoteList anecdotes={this.state.anecdotes} /> } />
